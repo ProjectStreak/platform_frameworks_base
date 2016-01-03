f934863562d1593c7bb28e26bf3c1f9048788f5c
maxwen <max.weninger@gmail.com>
2016-01-03 11:52:48 +0530

    Add auto brightness toggle to Quick Settings
    - Not a new tile, just a small icon/toggle beside the brightness slider
    
    Syberia: revert 1f2484b2f790b2f1cd80bda12d75c263331314a6 before
    
    Change-Id: I9632532aa3558911e6e7c8718feddc83e7bcfac6
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1191,6 +1191,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
+                    mContext,
                     mNotificationShadeWindowView,
                     mNotificationPanelViewController,
                     mNotificationShadeDepthControllerLazy.get(),
