76f8f4ffb2aea8d64ad673cd07cd2b2f1d5c5a91
martincz <martincz@mokeedev.com>
2018-09-11 04:03:36 -0500

    base: StreakUtils: Add check for Chinese Language
    As dumb as this sounds, this is actually useful for a few mods such as
    the carrier label and special captive portal for chinese users.
    
    Change-Id: Ida10723ec3a17bad28c9f6c4d4c73ec3e9a9a981

--- core/java/com/android/internal/util/streak/StreakUtils.java
+++ core/java/com/android/internal/util/streak/StreakUtils.java
@@ -21,11 +21,14 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.NetworkInfo;
 import android.os.SystemProperties;
 
 import com.android.internal.R;
+
+import java.util.Locale;
 
 public class StreakUtils {
 
@@ -96,5 +99,11 @@ public class StreakUtils {
     // Check to see if device supports A/B (seamless) system updates
     public static boolean isABdevice(Context context) {
         return SystemProperties.getBoolean("ro.build.ab_update", false);
+    }
+
+    // Check for Chinese language
+    public static boolean isChineseLanguage() {
+       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
+               Locale.CHINESE.getLanguage());
     }
 }
