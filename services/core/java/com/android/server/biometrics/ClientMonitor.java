856230e0b79810fe34988c46d0cdb3f1a7617aa7
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-09-20 19:52:53 +0000

    Disable fingerprint vibration if no haptics
    - Follow global haptic feedbacks setting to disable fingerprint authentication vibration
      on success.
    
    - This doesn't affect vibration on error.
    
    Change-Id: Id0335536d4db3a907b640bd899716bb69f86abb2
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- services/core/java/com/android/server/biometrics/ClientMonitor.java
+++ services/core/java/com/android/server/biometrics/ClientMonitor.java
@@ -298,9 +298,14 @@ public abstract class ClientMonitor extends LoggableMonitor implements IBinder.D
     }
 
     public final void vibrateSuccess() {
-        Vibrator vibrator = mContext.getSystemService(Vibrator.class);
-        if (vibrator != null) {
-            vibrator.vibrate(mSuccessVibrationEffect, FINGERPRINT_SONFICATION_ATTRIBUTES);
+        final boolean isHapticEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+        if (isHapticEnabled) {
+            Vibrator vibrator = mContext.getSystemService(Vibrator.class);
+            if (vibrator != null) {
+                vibrator.vibrate(mSuccessVibrationEffect, FINGERPRINT_SONFICATION_ATTRIBUTES);
+            }
         }
     }
 
