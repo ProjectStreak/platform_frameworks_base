882d9781505663bc64e9a2dcc04c4ba7bae7fc53
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-11 13:36:13 +0300

    SystemUI: QSPanel: Do not reassign long click listener
    Breaks long click for quick qs header
    introduced in: f1ed9ba810ac
    
    Change-Id: I788882041ee771ceffdb53bb15c6991420cd19b5

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -1214,19 +1214,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private void configureTile(QSTile t, QSTileView v) {
         if (mTileLayout != null) {
             v.setHideLabel(!mTileLayout.isShowTitles());
-            if (t.isDualTarget()) {
-                if (!mTileLayout.isShowTitles()) {
-                    v.setOnLongClickListener(view -> {
-                        t.secondaryClick();
-                        return true;
-                    });
-                } else {
-                    v.setOnLongClickListener(view -> {
-                        t.longClick();
-                        return true;
-                    });
-                }
-            }
         }
     }
 
