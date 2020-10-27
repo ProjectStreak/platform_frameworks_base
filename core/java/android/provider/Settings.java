8545544921ff1c1684db71281bd068a6492441a5
HSgod <hsgodkontakt@gmail.com>
2020-10-27 21:08:08 +0000

    Cleanup network traffic
    Revert "NetworkTraffic: Update for Android R"
    
    This reverts commit d0ee59f90d55d10dd70847497971452ac8a7b735.
    
    Revert "NetworkTraffic: Hide arrows in statusbar [1/2]"
    
    This reverts commit e9643c64653d4a74538ef897667e7fdc8e4df6ef.
    
    Revert "NetworkTraffic: Make the indicator arrows dynamic"
    
    This reverts commit 90a3e5d1a46c059012c392abe256f1b050018559.
    
    Revert "NetworkTraffic: Switch to OOS style indicator"
    
    This reverts commit aa4053698f0fb6dd98bc69ea0cb9768d642344ce.
    
    Revert "NetworkTraffic: Fix indicator not hiding on lost connection"
    
    This reverts commit efdf1dbbcdcbaec6d6ed59fd370158d77737d939.
    
    Revert "NetworkTraffic: Fix static color usage"
    
    This reverts commit ed48871e651553ca6a5274b8485092b697b56568.
    
    Revert "NetworkTraffic: Add it properly to statusbar icons"
    
    This reverts commit e3ba8b345eb04b17c83ae2a56099aa0012e3ff89.
    
    Revert "NetworkTraffic: Skip more useless handler calls"
    
    This reverts commit 3efdc3d7ec998596818d54e942c62663356207c5.
    
    Revert "NetworkTraffic: Stop the handler if screen is off"
    
    This reverts commit eca8999cf7996514f898647352437c94eb48c359.
    
    Revert "NetworkTraffic: Fix janky traffic indicator tint"
    
    This reverts commit c1b34284428611a6a83e934e81efdf88f893f805.
    
    Revert "base: Statusbar network traffic [1/2]"
    
    This reverts commit 385f5647fec6d5c885452c36b95d019cf1db077e.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5008,24 +5008,6 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
-         * Wheter to show network traffic indicator in statusbar
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
-
-        /**
-         * Network traffic inactivity threshold (default is 1 kBs)
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
-
-        /**
-         * Whether to disable showing arrows in statusbar network traffic indicators
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
-
-        /**
          * Swipe to screenshot gesture
          * Also known as Three Fingers Screenshot.
          * @hide
